faf64921280a20985623533e4a4b92b56627bc51
NathanielEllsworth <nathanielellsworth@gmail.com>
2017-05-29 12:57:40 -0400

    finished Getting started with programming, 6% done

--- assets/javascript/practice.js
+++ assets/javascript/practice.js
@@ -47,31 +47,110 @@
 // }
 
 /**
- * More practice with conditionals
+ * Math and the modulo
+ */
+
+// // Below is an example of printing the remainder of 18/4 using modulo:
+// // console.log(18 % 4);
+// console.log(14%3);
+// console.log(99%8);
+// console.log(11%3);
+
+/**
+ * Modulo and if / else
+ */
+
+// //An example of an if/else statement with modulo in the condition
+//
+//
+//
+// if( 10 % 2 === 0) {
+//     console.log("The first number is even");
+// } else {
+//     console.log("The first number is odd");
+// }
+
+
+/**
+ * Substrings
+ */
+
+// // Be careful with the substring's letter positions!
+// "wonderful day".substring(3,7);
+
+
+/**
+ * More substring practice
+ */
+
+// // Use console.log( ) to print out the substrings.
+// // Here is an example of the 1st to 4th letter of "JavaScript":
+// // console.log("JavaScript".substring(0,4));
+// console.log("January".substring(0,3));
+// console.log("Melbourne is great".substring(0,12));
+// console.log("Hamburgers".substring(3,10));
+
+
+/**
+ * Variables
  */
 
 
+// // To create a variable, we use only one equals sign
+// // But to check if two values are equal, we use 3 equal signs.
+//
+// // declare your variable here:
+// var myAge = 27;
+//
+// console.log(myAge);
 
 
+/**
+ * More Variable Practice
+ */
 
 
+// // Declare a variable on line 3 called
+// // myCountry and give it a string value.
+// var myCountry = "America";
+//
+// // Use console.log to print out the length of the variable myCountry.
+// console.log(myCountry.length );
+//
+// // Use console.log to print out the first three letters of myCountry.
+// console.log(myCountry.substring(0,3) );
 
 
+/**
+ * Change variable values
+ */
+
+// // On line 2, declare a variable myName and give it your name.
+// var myName = "Nate";
+// // On line 4, use console.log to print out the myName variable.
+// console.log(myName);
+// // On line 7, change the value of myName to be just the first 2
+// // letters of your name.
+// console.log(myName.substring(0,2));
+// // On line 9, use console.log to print out the myName variable.
+// console.log("myName");
 
 
+/**
+ * Conclusion: Part 2
+ */
 
 
-
-
-
-
-
-
-
-
-
-
-
+// // Not sure where to begin? Check the Hint!
+//
+// var myName = "Nathaniel"
+//
+// if( "myName".length >= 7 ) {
+//     console.log("I did not finished my first course!");
+// }
+// else {
+//     console.log("I finished my first course!");
+// }
 
 
 
