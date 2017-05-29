907e78d9415196175261f844cd8448fe1739d1a5
NathanielEllsworth <nathanielellsworth@gmail.com>
2017-05-29 12:12:38 -0400

    added jQuery practice

---
+++ assets/javascript/practice.js
@@ -0,0 +1,96 @@
+/**
+ *
+ * Code-Cadamy JavaScript course
+ *
+ * 5/29/2017
+ *
+ * '   ' Hours to complete
+ *
+ *
+ */
+
+
+//$(document).ready(function(){
+
+
+/**
+ * Comparisons
+ */
+
+// // Here is an example of using the greater than (>) operator.
+//     console.log(15 !== 4); // 15 > 4 evaluates to true, so true is printed.
+//
+// // Fill in with >, <, === so that the following print out true:
+//     console.log("Xiao Hui".length <= 122);
+//     console.log("Goody Donaldson".length >= 8);
+//     console.log(8*2 === 16);
+
+
+/**
+ * Decisions, decisions
+ */
+
+// if ( "Nathaniel".length >= 7 ) {
+//     console.log("You have a long name!" );
+// }
+
+
+/**
+ * Computers are smart
+ */
+
+// if( "myName".length >= 7 ) {
+//     console.log("You have a long name!");
+// }
+// else {
+//     console.log("You have a short name!");
+// }
+
+/**
+ * More practice with conditionals
+ */
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//});
