957f0eec14776df3223bcea6e6a79597cf3bc0d6
NathanielEllsworth <nathanielellsworth@gmail.com>
2017-05-29 16:48:08 -0400

    25% complete

--- assets/javascript/practice.js
+++ assets/javascript/practice.js
@@ -458,25 +458,161 @@ var sleepCheck = function (numHours){
 
 
 
+/**
+ * Computer Choice: Part 1
+ */
+
+var userChoice = prompt("Do you choose rock, paper or scissors?");
+
+var computerChoice = Math.random();
+console.log(computerChoice);
 
 
 
 
+/**
+ * Computer Choice: Part 2
+ */
+
+var userChoice = prompt("Do you choose rock, paper or scissors?");
+
+var computerChoice = Math.random();
+//console.log(computerChoice);
+
+if (computerChoice < 0.33) {
+    computerChoice = "rock";
+}
+else if (computerChoice < 0.66) {
+    computerChoice = "paper";
+}
+else {
+    computerChoice = "scissors";
+}
+
+
+/**
+ * Both choices are the same!
+ */
+
+
+/*var userChoice = prompt("Do you choose rock, paper or scissors?");
+ var computerChoice = Math.random();
+ if (computerChoice < 0.34) {
+ computerChoice = "rock";
+ } else if(computerChoice <= 0.67) {
+ computerChoice = "paper";
+ } else {
+ computerChoice = "scissors";
+ } console.log("Computer: " + computerChoice);*/
+
+var compare = function(choice1, choice2){
+    if(choice1 === choice2) {
+        return "The result is a tie!";
+    }
+};
+
+
+/**
+ * What if choice-1 is paper?
+ */
+
+/*var userChoice = prompt("Do you choose rock, paper or scissors?");
+ var computerChoice = Math.random();
+ if (computerChoice < 0.34) {
+ computerChoice = "rock";
+ } else if(computerChoice <= 0.67) {
+ computerChoice = "paper";
+ } else {
+ computerChoice = "scissors";
+ } console.log("Computer: " + computerChoice);*/
+
+var compare = function (choice1, choice2) {
+    if (choice1 === choice2) {
+
+        return "The result is a tie!";
+
+    } else if (choice1 === "rock") {
+
+        if (choice2 === "scissors") {
+
+            return "rock wins";
+
+        } else {
+
+            return "paper wins";
+        }
+
+    } else if (choice1 === "paper") {
+
+        if (choice2 === "rock") {
+
+            return "paper wins";
+
+        } else {
+
+            return "scissors wins";
+
+        }
+    }
+};
+
+
+/**
+ * Next steps
+ */
+
+
+var userChoice = prompt("Do you choose rock, paper or scissors?");
+var computerChoice = Math.random();
+if (computerChoice < 0.34) {
+    computerChoice = "rock";
+} else if(computerChoice <= 0.67) {
+    computerChoice = "paper";
+} else {
+    computerChoice = "scissors";
+} console.log("Computer: " + computerChoice);
+
+var compare = function (choice1, choice2) {
+
+    if (choice1 === choice2) {
+        return "The result is a tie!";
+    }
+
+    else if (choice1 === "rock") {
+        if (choice2 === "scissors") {
+            return "rock wins";
+        }
+        else {
+            return "paper wins";
+        }
+    }
+
+    else if (choice1 === "paper") {
+        if (choice2 === "rock") {
+            return "paper wins";
+        }
+        else {
+            return "scissors wins";
+        }
+    }
+
+    else (choice1 === "scissors")
+    {
+        if (choice2 === "paper") {
+            return "scissors wins";
+        }
+        else {
+            return "rock wins";
+        }
+    }
+};
+compare(userChoice, computerChoice);
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
+/**
+ * ------------------------------------------------------------------------------------
+ */
 
 
 
